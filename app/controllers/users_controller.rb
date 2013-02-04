866205ec2038d0231ebab7923a9e21e6576ab266
Marc Rossi <rossmarc@gmail.com>
2013-02-04 16:52:08 +0100

    fin users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
+    @title = "s'inscrire"
     @user = User.new
 
     respond_to do |format|
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :nom
+  
+  attr_accessible(:email, :nom)
   has_many :microposts
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   
+  validates(:nom, :presence => true,
+                  :length => { :maximum => 50 }
+            )
+  validates(:email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+            )
 end
